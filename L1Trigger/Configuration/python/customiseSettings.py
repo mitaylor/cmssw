c15be832c8a3ba61ac2055ffee456c3545481167
mitaylor <mitay@mit.edu>
2023-10-17 14:58:02 +0200

    Add update caloParams with jet seed threshold 2.5 GeV

--- L1Trigger/Configuration/python/customiseSettings.py
+++ L1Trigger/Configuration/python/customiseSettings.py
@@ -2,6 +2,10 @@ from __future__ import print_function
 import os.path
 import FWCore.ParameterSet.Config as cms
 
+def L1TSettingsToCaloParamsHI_2023_v0_4_2(process):
+    process.load("L1Trigger.L1TCalorimeter.caloParamsHI_2023_v0_4_2_cfi")
+    return process
+
 def L1TSettingsToCaloParamsHI_2023_v0_4_1(process):
     process.load("L1Trigger.L1TCalorimeter.caloParamsHI_2023_v0_4_1_cfi")
     return process
